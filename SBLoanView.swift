93508664bc3ff18c5a9ce775b987713f57f719a3
diegoferreiracaetano <di.cafe@hotmail.com>
2025-04-20 11:35:48 -0300

    first commit

---
+++ SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods-SBLoan_Example
+//
+//  Created by Diego Ferreira Caetano on 09/04/25.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
